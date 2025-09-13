fc0a0c302d6604aa529fe040efb0e7a3f1d650b8
Anton Ryabov <antonryabovdev@gmail.com>
2025-09-13 12:15:29 +0300

    feat: solve 2 problems

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -174,8 +174,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -195,8 +195,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (i !== n && n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
